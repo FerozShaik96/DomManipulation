decf89e7988a59afe5920df2fcae3babdf854c57
FerozShaik96 <ferozshaik1997@gmail.com>
2023-02-17 16:06:34 +0530

    HeaderTitle-Change

--- Dom-Basics/DomBasic.js
+++ Dom-Basics/DomBasic.js
@@ -1,6 +1,16 @@
 `use strict`;
-//Examine the document object
-// console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
+// //Examine the document object
+// // console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links);
+
+let headTitle = (document.getElementById("header-title").textContent =
+  "Welcome Feroz");
