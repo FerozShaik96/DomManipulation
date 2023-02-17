772660d7ab70310151954bddea806cbc4590932f
FerozShaik96 <ferozshaik1997@gmail.com>
2023-02-17 16:43:45 +0530

    chnaged the stylingto ADD ITEM

--- Dom-Basics/DomBasic.js
+++ Dom-Basics/DomBasic.js
@@ -12,5 +12,15 @@
 // console.log(document.forms);
 // console.log(document.links);
 
-let headTitle = (document.getElementById("header-title").textContent =
-  "Welcome Feroz");
+let headTitle = document.getElementById("header-title");
+let header = document.getElementById("main-header");
+const addItem = document.getElementsByClassName("title");
+// console.log(headTitle.textContent);
+//textContent will show the exact the content of that tag it wont care about the styleings
+// console.log(headTitle.innerText);
+// But this innerText will consider the styling and show the content
+// headTitle.innerText = "You are Alone";
+// headTitle.innerHTML = "<h3>Hello</h3>";
+header.style.borderBottom = "solid 3px black";
+addItem[0].style.fontWeight = "bold";
+addItem[0].style.color = "green";
