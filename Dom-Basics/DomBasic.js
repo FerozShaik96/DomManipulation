89ec6c41b500e4ba74a99bd7917d80b77bff7d2a
FerozShaik96 <ferozshaik1997@gmail.com>
2023-02-19 11:39:09 +0530

    Added New Functionality remove And Edit buttton

--- Dom-Basics/DomBasic.js
+++ Dom-Basics/DomBasic.js
@@ -58,58 +58,116 @@
 //   el.style.backgroundColor = "green";
 // }
 
-//Traversing The DOM
-let itemList = document.querySelector("#items");
-// ParentNode
-console.log(itemList.parentNode);
-itemList.parentNode.style.backgroundColor = "#f4f4f4";
-// LastelementChild
-console.log(itemList.lastElementChild.innerText);
-//Last Child
-console.log(itemList.lastChild);
-//FirstElementChild
-console.log(itemList.firstElementChild);
-//FirstChild
-console.log(itemList.firstChild);
-//nextSibling
-console.log(itemList.nextSibling);
-//NextElementSibling
-console.log(itemList.nextElementSibling);
-//PerviousSibling
-console.log(itemList.previousSibling);
-//PerviousElementSibling
-console.log(itemList.previousElementSibling);
+// //Traversing The DOM
+// let itemList = document.querySelector("#items");
+// // ParentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = "#f4f4f4";
+// // LastelementChild
+// console.log(itemList.lastElementChild.innerText);
+// //Last Child
+// console.log(itemList.lastChild);
+// //FirstElementChild
+// console.log(itemList.firstElementChild);
+// //FirstChild
+// console.log(itemList.firstChild);
+// //nextSibling
+// console.log(itemList.nextSibling);
+// //NextElementSibling
+// console.log(itemList.nextElementSibling);
+// //PerviousSibling
+// console.log(itemList.previousSibling);
+// //PerviousElementSibling
+// console.log(itemList.previousElementSibling);
 
-// CreateElement
-let newDiv = document.createElement("div");
-newDiv.className = "Hello";
-newDiv.id = "hello1";
-console.log(newDiv);
-//SetAttribute
-newDiv.setAttribute("title", "HELLo");
+// // CreateElement
+// let newDiv = document.createElement("div");
+// newDiv.className = "Hello";
+// newDiv.id = "hello1";
+// console.log(newDiv);
+// //SetAttribute
+// newDiv.setAttribute("title", "HELLo");
 
-//CreatTextNode
-let newDivText = document.createTextNode("HELLo");
-// Add Text To Div (AppendChild)
-newDiv.appendChild(newDivText);
+// //CreatTextNode
+// let newDivText = document.createTextNode("HELLo");
+// // Add Text To Div (AppendChild)
+// newDiv.appendChild(newDivText);
 
-//Insert newDiv in HTML
-let container = document.querySelector("header .container");
-let h1 = document.querySelector("header h1");
-container.insertBefore(newDiv, h1);
+// //Insert newDiv in HTML
+// let container = document.querySelector("header .container");
+// let h1 = document.querySelector("header h1");
+// container.insertBefore(newDiv, h1);
 
-let newDiv1 = document.createElement("div");
-newDiv1.className = "Hello";
-newDiv1.id = "hello1";
-console.log(newDiv1);
-//SetAttribute
-newDiv1.setAttribute("title", "HELLo");
+// let newDiv1 = document.createElement("div");
+// newDiv1.className = "Hello";
+// newDiv1.id = "hello1";
+// console.log(newDiv1);
+// //SetAttribute
+// newDiv1.setAttribute("title", "HELLo");
 
-//CreatTextNode
-let newDiv1Text = document.createTextNode("HEllo");
-// Add Text To Div (AppendChild)
-newDiv1.appendChild(newDiv1Text);
-let divId = document.querySelector("div #main");
-let h2 = document.querySelector("div h2");
-console.log(h2);
-divId.insertBefore(newDiv1, h2);
+// //CreatTextNode
+// let newDiv1Text = document.createTextNode("HEllo");
+// // Add Text To Div (AppendChild)
+// newDiv1.appendChild(newDiv1Text);
+// let divId = document.querySelector("div #main");
+// let h2 = document.querySelector("div h2");
+// console.log(h2);
+// divId.insertBefore(newDiv1, h2);
+
+let addForm = document.getElementById("addForm");
+let itemList = document.getElementById("items");
+
+//Form Submit Event
+addForm.addEventListener("submit", additem);
+// Delete Event
+itemList.addEventListener("click", removeItem);
+//
+function additem(e) {
+  e.preventDefault();
+
+  //Get Input Value
+  let newItem = document.getElementById("item").value;
+
+  //Creat new Li Element
+  let li = document.createElement("li");
+
+  //Adding a classto it
+  li.className = "list-group-item";
+  console.log(li);
+  //Adding a The Text
+  li.appendChild(document.createTextNode(newItem));
+
+  //Delete Button
+  let button = document.createElement("button");
+
+  //Adding Classes
+  button.className = "btn btn-danger btn-sm float-right delete";
+
+  //Appending Text Node
+  button.appendChild(document.createTextNode("X"));
+  //Appending button to li
+  li.appendChild(button);
+
+  //Creating A New Edit Button
+  let editButton = document.createElement("button");
+  //Adding The Classes to it
+  editButton.classList = "btn btn-danger btn-sm float-right";
+  //Adding the Text Content
+  editButton.appendChild(document.createTextNode("Edit"));
+  //Appending to List
+  li.appendChild(editButton);
+
+  itemList.appendChild(li);
+}
+
+//Remove Item
+
+function removeItem(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure..?")) {
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
