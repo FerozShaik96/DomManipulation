b9418a05f7079fd3141e3d675c879450210a6610
FerozShaik96 <ferozshaik1997@gmail.com>
2023-03-12 21:09:28 +0530

    async_Promise.all

--- promises_&_Asynch_Await/async_await.js
+++ promises_&_Asynch_Await/async_await.js
@@ -7,27 +7,39 @@ const preMovie = async () => {
       res("ticket");
     }, 3000);
   });
+  const getPopcorn = new Promise((resolve, reject) => resolve("popcorn"));
+  const getCandy = new Promise((resolve, reject) => resolve("Candy"));
+  const getcoke = new Promise((resolve, reject) => resolve("Cool Drink"));
+
   let ticket = await promiseWifeBringingTickets;
-  const getPopCorn = new Promise((res, rej) => res(` PopCorn `));
-  const getButter = new Promise((res, rej) => res(` Buttor`));
-  const getColdDrinks = new Promise((res, rej) => res(`Cold Drinks`));
-  console.log("Wife: I Got Tickets");
-  console.log("Husband: We Should Go in");
-  console.log("Wife: No i am hungry");
-  let popcorn = await getPopCorn;
-  console.log(`Husband: Here It is the ${popcorn}. We Should Go in`);
-  console.log("Wife: I need Butter On my PopCorn");
-  let butter = await getButter;
-  console.log(`Husband: Here It is the ${butter}. We Should Go in`);
-  console.log("Husband: So do you Need any thing Else ...!");
-  let cold = await getColdDrinks;
-  console.log("Wife: I need A Cold Drink");
-  console.log(
-    `Husband: Here Is the ${cold} .. Do you need Any thing Else....Bab🧡`
-  );
-  console.log(
-    "Wife: Thats Enough ... Ohh We are getting Late For the  Movie. Lets Go"
-  );
+  let [popCorn, candy, drink] = await Promise.all([
+    getPopcorn,
+    getCandy,
+    getcoke,
+  ]);
+  console.log(`${popCorn},${candy} ${drink}`);
+  //   const getPopCorn = new Promise((res, rej) => res(` PopCorn `));
+  //   const getButter = new Promise((res, rej) => res(` Buttor`));
+  //   const getColdDrinks = new Promise((res, rej) => res(`Cold Drinks`));
+  //   console.log("Wife: I Got Tickets");
+  //   console.log("Husband: We Should Go in");
+  //   console.log("Wife: No i am hungry");
+  //   let popcorn = await getPopCorn;
+  //   console.log(`Husband: Here It is the ${popcorn}. We Should Go in`);
+  //   console.log("Wife: I need Butter On my PopCorn");
+  //   let butter = await getButter;
+  //   console.log(`Husband: Here It is the ${butter}. We Should Go in`);
+  //   console.log("Husband: So do you Need any thing Else ...!");
+  //   let cold = await getColdDrinks;
+  //   console.log("Wife: I need A Cold Drink");
+  //   console.log(
+  //     `Husband: Here Is the ${cold} .. Do you need Any thing Else....Bab🧡`
+  //   );
+  //   console.log(
+  //     "Wife: Thats Enough ... Ohh We are getting Late For the  Movie. Lets Go"
+  //   );
+
+  // Handeling All Promises
   return ticket;
 };
 preMovie().then((m) => console.log(`Person 3 : show ${m}`));
